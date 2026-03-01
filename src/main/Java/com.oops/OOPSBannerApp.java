e9e6e172e783ec19ab6671f009a78515e8148433
Deb Jyoti Ghosh <debjyoti08adamas@gmail.com>
2026-03-01 16:38:59 +0530

    UC6: Use static methods for O P S patterns

--- src/main/Java/com.oops/OOPSBannerApp.java
+++ src/main/Java/com.oops/OOPSBannerApp.java
@@ -1,18 +1,61 @@
+/**
+ * OOPS Banner App - Use Case 6 (UC6)
+ * Uses static helper methods to generate banner patterns for O, P, S
+ * and composes "OOPS" without hardcoding full lines in main.
+ *
+ * @author Deb Jyoti Ghosh
+ * @version 6.0
+ */
 public class OOPSBannerApp {
+
+    private static String[] getO() {
+        return new String[] {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    private static String[] getP() {
+        return new String[] {
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    private static String[] getS() {
+        return new String[] {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join("", "*     *", " *     *", " *     *", " *     *"),
-            String.join("", "*     *", " *     *", " *     *", " *      "),
-            String.join("", "*     *", " *     *", " *     *", "  ***** "),
-            String.join("", "*     *", " *     *", " *     *", "      * "),
-            String.join("", "*     *", " *     *", " *     *", " *    * "),
-            String.join("", " ***** ", " ***** ", " ***** ", " ***** ")
-        };
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
 
-        for(String line : banner){
+        String[] banner = new String[7];
+        for (int i = 0; i < banner.length; i++) {
+            banner[i] = String.join(" ", o[i], o[i], p[i], s[i]);
+        }
+
+        for (String line : banner) {
             System.out.println(line);
         }
     }
