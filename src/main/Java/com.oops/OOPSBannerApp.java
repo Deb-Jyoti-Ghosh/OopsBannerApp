39e600ca17c2b80add67e8429a3585aa04694a8e
Deb Jyoti Ghosh <dg9044@srmist.edu.in>
2026-03-01 16:33:34 +0530

    Update OOPSBannerApp.java

--- src/main/Java/com.oops/OOPSBannerApp.java
+++ src/main/Java/com.oops/OOPSBannerApp.java
@@ -2,17 +2,17 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-                String.join("", " ***** ", " ***** ", " ***** ", " ***** "),
-                String.join("", "*     *", " *     *", " *     *", " *     *"),
-                String.join("", "*     *", " *     *", " *     *", " *      "),
-                String.join("", "*     *", " *     *", " *     *", "  ***** "),
-                String.join("", "*     *", " *     *", " *     *", "     *  "),
-                String.join("", "*     *", " *     *", " *     *", " *   *  "),
-                String.join("", " ***** ", " ***** ", " ***** ", " ***** ")
-        };
+        String[] banner = new String[7];
 
-        for (String line : bannerLines) {
+        banner[0] = String.join("", " ***** ", " ***** ", " ***** ", " ***** ");
+        banner[1] = String.join("", "*     *", " *     *", " *     *", " *     *");
+        banner[2] = String.join("", "*     *", " *     *", " *     *", " *      ");
+        banner[3] = String.join("", "*     *", " *     *", " *     *", "  ***** ");
+        banner[4] = String.join("", "*     *", " *     *", " *     *", "      * ");
+        banner[5] = String.join("", "*     *", " *     *", " *     *", " *    * ");
+        banner[6] = String.join("", " ***** ", " ***** ", " ***** ", " ***** ");
+
+        for(String line : banner) {
             System.out.println(line);
         }
     }
